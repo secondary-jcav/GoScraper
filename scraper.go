4081dadc7c0938408c655fbf2623d6989b79f5ee
Julio Arellano <secondary.jcav@gmail.com>
2022-02-27 17:17:36 +0100

    added dependencies, visit link through gocolly

--- scraper.go
+++ scraper.go
@@ -1,1 +1,19 @@
 package main
+
+import (
+	"fmt"
+
+	"github.com/gocolly/colly"
+)
+
+func main() {
+	c := colly.NewCollector(
+		colly.AllowedDomains("en.wikipedia.org"),
+	)
+
+	c.OnHTML(".mw-parser-output", func(e *colly.HTMLElement) {
+		links := e.ChildAttrs("a", "href")
+		fmt.Println(links)
+	})
+	c.Visit("https://en.wikipedia.org/wiki/Web_scraping")
+}
